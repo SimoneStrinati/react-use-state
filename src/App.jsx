b440dad7d4d7c27eacfb22ab940d9f5cede51143
simonestrinati <simone.strinati@outlook.it>
2026-01-17 11:27:18 +0100

    creazione struttura e css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,23 @@ function App() {
 
   return (
     <>
- 
+
+      <div className='container'>
+        <h1>Learn Web Development</h1>
+
+        <div className='btnContainer'>
+          <button className='activeBtn'>HTML</button>
+
+        </div>
+
+        <div className='card'>
+          <h2>titolo</h2>
+          <p>contenuto</p>
+
+        </div>
+
+      </div>
+
     </>
   )
 }
